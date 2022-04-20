eabc3b05840fc967c206d7e57ba56146cf87477f
stevenvits <steven.vits@ectosense.com>
2022-04-20 11:43:49 +0200

    commit before opening branch exc 1

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def MoveElevator(brackets):
+
+    floor = 0
+
+    for el in brackets:
+        if(el == '('):
+            floor = floor + 1
+        elif(el == ')'):
+            floor = floor - 1
+        else:
+            raise Exception('The input contained an invalid command character')
+
+    return floor
+
+floor = MoveElevator('(()')
+print(floor)
