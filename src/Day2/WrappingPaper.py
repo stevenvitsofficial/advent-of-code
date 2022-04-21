b7abf17a93b8d3b16208452c4ba1056b704fd95e
stevenvits <steven.vits@ectosense.com>
2022-04-21 11:28:41 +0200

    added additional unit tests

--- src/Day2/WrappingPaper.py
+++ src/Day2/WrappingPaper.py
@@ -1,8 +1,15 @@
 def OrderPaper(width, height, length):
 
     dim = [width, height, length]
+
+    if(not all([(isinstance(item, float)  or isinstance(item, int)) for item in dim])):
+        raise Exception('The input does not consist of numbers only')
+    elif(any(item <= 0 for item in dim)):
+        raise Exception('The input has negative or 0 numbers')
+
     dim.sort()
 
     paperorder = 2*(dim[0]*dim[1]+dim[1]*dim[2]+dim[0]*dim[2])+dim[0]*dim[1]
 
     return paperorder
+
